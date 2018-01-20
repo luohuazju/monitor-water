16e9312a6d035fc4a5b0094cc8eb1c6701adb8f1
Carl Luo <cluo@jobs2careers.com>
2018-01-20 00:15:38 -0600

    scrawl the daba water infor

--- scrapy-clawer/scrapy_clawer/scrapy_clawer/spiders/open_url.py
+++ scrapy-clawer/scrapy_clawer/scrapy_clawer/spiders/open_url.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-import os
 
 path2phantom = '/opt/phantomjs/bin/phantomjs'
 browser = webdriver.PhantomJS(path2phantom)
@@ -7,11 +6,8 @@ browser.get('https://hydromet.lcra.org/riverreport')
 
 tables = browser.find_elements_by_css_selector('table.table-condensed')
 
-#print tables[5].text
 tbody = tables[5].find_element_by_tag_name("tbody")
-#print tbody.text
 for row in tbody.find_elements_by_tag_name("tr"):
-    #print row.text
     cells = row.find_elements_by_tag_name("td")
     if(cells[0].text == 'Marble Falls (Starcke)'):
         print cells[1].text
