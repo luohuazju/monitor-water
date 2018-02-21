4ac38428d217d534013994197041aaa3f980497a
Carl Luo <cluo@jobs2careers.com>
2018-02-20 23:46:30 -0600

    commit my go project

--- restful_go_api/src/com/sillycat/restful_go_api/main.go
+++ restful_go_api/src/com/sillycat/restful_go_api/main.go
@@ -1,63 +1,34 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-)
+import "github.com/gin-gonic/gin"
+import "com/sillycat/restful_go_api/waterrecord"
 
 var DB = make(map[string]string)
 
 func setupRouter() *gin.Engine {
 	// Disable Console Color
 	// gin.DisableConsoleColor()
-	r := gin.Default()
+	router := gin.Default()
 
 	// Ping test
-	r.GET("/ping", func(c *gin.Context) {
+	router.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong")
 	})
 
-	// Get user value
-	r.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		value, ok := DB[user]
-		if ok {
-			c.JSON(200, gin.H{"user": user, "value": value})
-		} else {
-			c.JSON(200, gin.H{"user": user, "status": "no value"})
-		}
-	})
+	v1 := router.Group("api/v1")
+	{
+		v1.GET("/instructions", waterrecord.GetInstructions)
+		v1.GET("/instructions/:id", waterrecord.GetInstruction)
+		v1.POST("/instructions", waterrecord.PostInstruction)
+		v1.PUT("/instructions/:id", waterrecord.UpdateInstruction)
+		v1.DELETE("/instructions/:id", waterrecord.DeleteInstruction)
+	}
 
-	// Authorized group (uses gin.BasicAuth() middleware)
-	// Same than:
-	// authorized := r.Group("/")
-	// authorized.Use(gin.BasicAuth(gin.Credentials{
-	//	  "foo":  "bar",
-	//	  "manu": "123",
-	//}))
-	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-		"foo":  "bar", // user:foo password:bar
-		"manu": "123", // user:manu password:123
-	}))
-
-	authorized.POST("admin", func(c *gin.Context) {
-		user := c.MustGet(gin.AuthUserKey).(string)
-
-		// Parse JSON
-		var json struct {
-			Value string `json:"value" binding:"required"`
-		}
-
-		if c.Bind(&json) == nil {
-			DB[user] = json.Value
-			c.JSON(200, gin.H{"status": "ok"})
-		}
-	})
-
-	return r
+	return router
 }
 
 func main() {
-	r := setupRouter()
+	router := setupRouter()
 	// Listen and Server in 0.0.0.0:8080
-	r.Run(":8080")
+	router.Run(":8080")
 }
