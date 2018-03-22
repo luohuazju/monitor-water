c92076c9e532cdf62995afe2c29be69ef7385a16
Hua Luo <hluo@machluo.local>
2018-03-22 09:36:59 -0500

    change the code to support headless

--- scrapy-clawer/scrapy_clawer/spiders/open_url.py
+++ scrapy-clawer/scrapy_clawer/spiders/open_url.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
 
-path2phantom = '/opt/phantomjs/bin/phantomjs'
-browser = webdriver.PhantomJS(path2phantom)
+options = webdriver.ChromeOptions()
+#options.binary_location = '/usr/bin/google-chrome-unstable'
+options.add_argument('headless')
+options.add_argument('window-size=1200x600')
+browser = webdriver.Chrome(chrome_options=options)
+
 browser.get('https://hydromet.lcra.org/riverreport')
 
 tables = browser.find_elements_by_css_selector('table.table-condensed')
@@ -10,6 +14,6 @@ tbody = tables[5].find_element_by_tag_name("tbody")
 for row in tbody.find_elements_by_tag_name("tr"):
     cells = row.find_elements_by_tag_name("td")
     if(cells[0].text == 'Marble Falls (Starcke)'):
-        print cells[1].text
+        print(cells[1].text)
 
 browser.quit()
